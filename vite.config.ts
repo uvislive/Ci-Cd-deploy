ff61b82b9ce27f4b4d45d59c07016cb7447ab6a1
uvislive <64905093+uvislive@users.noreply.github.com>
2025-06-16 00:04:47 +0530

    Updated code

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react ()],
-  base: '/Ci-Cd-deploy/', // <-- same as repo name
+  base: process.env.NODE_ENV === 'production' ? '/Ci-Cd-deploy/' : '/',
 })
